48bd9f461cd68a807ed94952d873f397ca0c0bed
cowboysj <su10jin11@khu.ac.kr>
2024-01-02 16:53:21 +0900

    :art: 반응형 세팅

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from "react";
 import { Provider } from "react-redux";
 import store from "./store";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import styled from "styled-components";
+
 function App() {
   return (
     <Provider store={store}>
-      <div className="App"></div>
+      <BackgroundWrap>
+        <Background>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </BrowserRouter>
+        </Background>
+      </BackgroundWrap>
     </Provider>
   );
 }
 
 export default App;
+
+const BackgroundWrap = styled.div`
+  height: calc(var(--vh, 1vh) * 100);
+  background: linear-gradient(180deg, #ffd25d 0%, #ff984b 100%);
+`;
+//Background : 모바일로 보이는 영역
+const Background = styled.div`
+  max-width: 680px;
+  height: calc(var(--vh, 1vh) * 100);
+  margin: 0px auto;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+`;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Home() {
+  return (
+    <div>
+      <Content>ㅇㅇ</Content>
+    </div>
+  );
+}
+
+const Content = styled.div`
+  background-color: pink;
+  width: 10%;
+`;
