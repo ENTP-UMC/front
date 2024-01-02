a14f9a620c5b400de6ffd6fcaa22b5fd921c4d3b
안혜연 <ahy020606@naver.com>
2024-01-02 23:17:22 +0900

    탭 틀 만듦

--- src/pages/Thunder.jsx
+++ src/pages/Thunder.jsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useState } from 'react';
+import Header from "../components/Header/Header";
 
 export default function Thunder() {
-  return <div>Thunder</div>;
+  const [activeTab, setActiveTab] = useState('Long-Term');
+  
+  const handleClick = (tab) => {
+    setActiveTab(tab);
+  }
+  
+  return (
+    <div>
+      <Header />
+      <h2 style={{color: 'black', fontSize: 25, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>UMC 스몰팟</h2>
+      <div>
+        <button onClick={() => handleClick('Long-Term')} style={{color: activeTab === 'Long-Term' ? 'red' : 'black'}}>Long-Term</button>
+        <button onClick={() => handleClick('Thunder')} style={{color: activeTab === 'Thunder' ? 'red' : 'black'}}>Thunder</button>
+      </div>
+      {activeTab === 'Long-Term' && <div>Long-Term</div>}
+      {activeTab === 'Thunder' && <div>Thunder</div>}
+    </div>
+  );
 }
