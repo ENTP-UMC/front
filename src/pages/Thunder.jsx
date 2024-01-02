1afd4f0e824bb83f709e2dc899e8543f291042cb
안혜연 <ahy020606@naver.com>
2024-01-03 00:03:54 +0900

    '탭변경'

--- src/pages/Thunder.jsx
+++ src/pages/Thunder.jsx
@@ -1,40 +1,48 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
-import Header from "../components/Header/Header";
-
-export default function Thunder() {
-  const [activeTab, setActiveTab] = useState('Long-Term');
-  
-  const handleClick = (tab) => {
-    setActiveTab(tab);
-  }
-  
-  return (
-    <div>
-      <Header />
-      <h2 style={{color: 'black', fontSize: 25, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>UMC 스몰팟</h2>
-      <div>
-        <button onClick={() => handleClick('Long-Term')} style={{color: activeTab === 'Long-Term' ? 'red' : 'black'}}>Long-Term</button>
-        <button onClick={() => handleClick('Thunder')} style={{color: activeTab === 'Thunder' ? 'red' : 'black'}}>Thunder</button>
-      </div>
-      {activeTab === 'Long-Term' && <div>Long-Term</div>}
-      {activeTab === 'Thunder' && <div>Thunder</div>}
-    </div>
-=======
-import React from "react";
 import styled from "styled-components";
 import Header from "../components/Header/Header";
+import { Nav } from 'react-bootstrap';
 
 const ThunderContainer = styled.div`
   padding-top: 4.3125rem; /* 헤더의 높이만큼 여백을 추가합니다. */
 `;
 
-export default function Home() {
+const SelectedTabText = styled.div`
+  color: black;
+  font-size: 20px;
+  font-family: 'Pretendard-Regular';
+  font-weight: 500;
+  word-wrap: break-word;
+`;
+
+const NotSelectedTabText = styled.div`
+  color: #B0B0B0; 
+  font-size: 20px;
+  font-family: 'Pretendard-Regular';
+  font-weight: 500;
+  word-wrap: break-word
+`;
+
+
+export default function Thunder() {
+  let [tab, setTab] = useState(0);
+
   return (
     <ThunderContainer>
       <Header />
       <h1>Thunder</h1>
+      <h2 style={{color: 'black', fontSize: 25, fontFamily: 'Inter', fontWeight: '600', wordWrap: 'break-word'}}>UMC 스몰팟</h2>
+      <Nav variant="tabs"  defaultActiveKey="link0">
+        <SelectedTabText selected={tab === 0}>
+          <Nav.Link onClick={()=>{setTab(0)}} eventKey="link0">Long-term</Nav.Link>
+        </SelectedTabText>
+        <NotSelectedTabText selected={tab === 1}>
+          <Nav.Link onClick={()=>{setTab(1)}} eventKey="link1">Thunder⚡</Nav.Link>
+        </NotSelectedTabText>
+      </Nav>
+      function TabContent({tab}){
+        [<div>내용0</div>, <div>내용1</div>][tab]
+      }
     </ThunderContainer>
->>>>>>> origin/develop
   );
 }
