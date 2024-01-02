b8fa1990296663acb4fc1588f460c15840f9187c
imi21123 <imi21123@naver.com>
2024-01-03 05:03:03 +0900

    :lipstick: SideProject 글 작성 페이지 레이아웃 완성

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import LoginStep4 from "./components/Login/LoginStep4";
 import Mypage2 from "./components/Mypage/Mypage2";
 import Mypage3 from "./components/Mypage/Mypage3";
 import Diary from "./pages/Diary";
+import SideProjectPost from "./pages/SideProjectPost";
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
               <Route path="/diary" element={<Diary />} />
 
               <Route path="/sideproject" element={<SideProject />} />
+              <Route path="/sideprojectPost" element={<SideProjectPost />} />
               <Route path="/study" element={<Study />} />
               <Route path="/thunder" element={<Thunder />} />
             </Routes>
---
+++ src/pages/SideProjectPost.jsx
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import "../style/SideProjectPost.css";
+
+export default function SideProjectPost() {
+  const [plan, setPlan] = useState(0);
+  const [front, setFront] = useState(0);
+  const [back, setBack] = useState(0);
+  const [design, setDesign] = useState(0);
+  const [etc, setEtc] = useState(0);
+
+  const handlePlanChange = (e) => {
+    setPlan(e.target.value);
+  };
+  const handleFrontChange = (e) => {
+    setFront(e.target.value);
+  };
+  const handleBackChange = (e) => {
+    setBack(e.target.value);
+  };
+  const handleDesignChange = (e) => {
+    setDesign(e.target.value);
+  };
+  const handleEtcChange = (e) => {
+    setEtc(e.target.value);
+  };
+
+  return (
+    <div className="sideProjectPost">
+      <div className="sideProjectPost__title">제목</div>
+      <input
+        className="title-input"
+        type="string"
+        placeholder="제목을 입력해주세요"
+      />
+
+      <div className="sideProjectPost__title">모집 분야 / 인원</div>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div className="number-input-container">
+          <span className="label" style={{ marginRight: "0.62rem" }}>
+            기획
+          </span>
+          <input
+            className="number-input"
+            type="number"
+            value={plan}
+            onChange={handlePlanChange}
+          />
+        </div>
+        <div className="number-input-container">
+          <span className="label" style={{ marginRight: "0.62rem" }}>
+            디자인
+          </span>
+          <input
+            className="number-input"
+            type="number"
+            value={design}
+            onChange={handleDesignChange}
+          />
+        </div>
+      </div>
+
+      <div
+        style={{ display: "flex", flexDirection: "row", marginTop: "0.5rem" }}
+      >
+        <div className="number-input-container">
+          <span className="label" style={{ marginRight: "0.62rem" }}>
+            프론트엔드
+          </span>
+          <input
+            className="number-input"
+            type="number"
+            value={front}
+            onChange={handleFrontChange}
+          />
+        </div>
+        <div className="number-input-container">
+          <span className="label" style={{ marginRight: "0.62rem" }}>
+            백엔드
+          </span>
+          <input
+            className="number-input"
+            type="number"
+            value={back}
+            onChange={handleBackChange}
+          />
+        </div>
+        <div className="number-input-container">
+          <span className="label" style={{ marginRight: "0.62rem" }}>
+            기타
+          </span>
+          <input
+            className="number-input"
+            type="number"
+            value={etc}
+            onChange={handleEtcChange}
+          />
+        </div>
+      </div>
+
+      <div className="sideProjectPost__title">
+        모집 분야 / 프론트엔드 / 백엔드 / 진행방식
+      </div>
+      <input className="title-input" type="string" placeholder="#태그" />
+
+      <div className="sideProjectPost__title">자세한 설명</div>
+      <input
+        className="info-input"
+        type="string"
+        placeholder={`사이드 프로젝트에 대해 간단히 소개해주세요`}
+      />
+
+      <div className="sideProjectPost__title">카카오톡 오픈채팅 링크</div>
+      <input
+        className="title-input"
+        type="string"
+        placeholder="오픈채팅 링크를 붙여넣어주세요."
+      />
+
+      <div className="sideProjectPost__btn">등록</div>
+    </div>
+  );
+}
